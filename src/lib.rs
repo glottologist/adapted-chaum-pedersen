ce4ea190a355c175a4efa0d0a4237c90f4de07a2
jason <jason@ridgway-taylor.co.uk>
2024-04-01 12:59:35 +0700

    feat: added task proto specification

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,11 @@
 pub mod errors;
+pub mod acp {
+    pub mod messages {
+        include!(concat!(env!("OUT_DIR"), "/acp.messages.rs"));
+    }
+}
 
+use acp::messages;
 #[cfg(test)]
 mod tests {
     use super::*;
